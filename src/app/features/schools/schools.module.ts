e233e56c9171d54bc7f5d64ca8f54eac5b42c199
Rezevoo <arazavi@identecsolutions.com>
2023-06-29 21:10:26 +0200

    remove battery details component

--- src/app/features/schools/schools.module.ts
+++ src/app/features/schools/schools.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SchoolsComponent } from './schools.component';
-import { BatteryDetailsComponent } from './components/battery-details/battery-details.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { SchoolBatteriesDataStore } from './store/school-batteries-data.store';
+import { MaterialDesignModule } from 'src/app/shared/materialdesign/materialdesign.module';
 
 @NgModule({
   declarations: [
     SchoolsComponent,
-    BatteryDetailsComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
+    MaterialDesignModule,
   ],
   exports: [
     SchoolsComponent,
